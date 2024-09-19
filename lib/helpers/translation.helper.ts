bba63447fe1050b81bf1af3a39c893adbb1c9c62
Phuong Vo <phuong.vt@hodfords.com>
2024-09-19 16:38:22 +0700

    feat: refactor eslint and upgrade dependencies

--- lib/helpers/translation.helper.ts
+++ lib/helpers/translation.helper.ts
@@ -7,11 +7,11 @@ import { IS_FULL_LANGUAGE } from '../constants/language-key.constant';
 import { TranslationService } from '../services/translation.service';
 import { RunInLanguageParams, TranslateOptions } from '../types';
 
-export function trans(key: string, options: TranslateOptions = {}) {
+export function trans(key: string, options: TranslateOptions = {}): string {
     return TranslationService.instance.translate(key, options);
 }
 
-export function currentLanguage() {
+export function currentLanguage(): string {
     return TranslationService.instance.getCurrentLanguage();
 }
 
@@ -19,19 +19,22 @@ export function isFullLanguages(): boolean {
     return !!CLS_TRANSLATION_NAMESPACE.get(IS_FULL_LANGUAGE);
 }
 
-export function defaultLanguage() {
+export function defaultLanguage(): string {
     return TranslationService.instance.getDefaultLanguage();
 }
 
-export function getLanguageByKey(key: string) {
+export function getLanguageByKey(key: string): string {
     return TranslationService.instance.getLanguageByKey(key);
 }
 
-export function destroyTranslationNamespace() {
+export function destroyTranslationNamespace(): void {
     return cls.destroyNamespace(CLS_TRANSLATION_NAMESPACE_NAME);
 }
 
-export function runInLanguage<T>(params: RunInLanguageParams, callback: (...args: any[]) => T): Promise<T> {
+export function runInLanguage<T>(
+    params: RunInLanguageParams,
+    callback: (...args: (string | object)[]) => T
+): Promise<T> {
     return CLS_TRANSLATION_NAMESPACE.runAndReturn(async () => {
         if (typeof params === 'string') {
             CLS_TRANSLATION_NAMESPACE.set(TranslationService.instance.getDefaultLanguageKey(), params);
@@ -45,7 +48,7 @@ export function runInLanguage<T>(params: RunInLanguageParams, callback: (...args
     });
 }
 
-export function runInFullLanguages<T>(callback: (...args: any[]) => T): Promise<T> {
+export function runInFullLanguages<T>(callback: (...args: (string | object)[]) => T): Promise<T> {
     return CLS_TRANSLATION_NAMESPACE.runAndReturn(async () => {
         CLS_TRANSLATION_NAMESPACE.set(IS_FULL_LANGUAGE, true);
 
--- scripts/publish.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-npm view @hodfords/nestjs-cls-translation@"$(node -p "require('./package.json').version")" version && echo "Package is already published" && exit 0 || true
-npm install
-npm run build
-cp README.md dist
-cd dist
-npm publish --access public
